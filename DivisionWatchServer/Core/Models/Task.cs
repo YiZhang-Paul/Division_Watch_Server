45ddc1bcdf1b7c19964961e100c3869be6f9a35e
YI ZHANG <scaccterran@gmail.com>
2021-01-02 07:32:19 -0500

    POM-11 added more details

--- DivisionWatchServer/Core/Models/Task.cs
+++ DivisionWatchServer/Core/Models/Task.cs
@@ -1,5 +1,7 @@
-using System.Collections.Generic;
+using Core.Enums;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Models
 {
@@ -7,11 +9,29 @@ namespace Core.Models
     {
         public string Name { get; set; }
         public Category Category { get; set; }
-        public string Priority { get; set; }
+        public Priority Priority { get; set; } = Priority.Low;
         public int Estimate { get; set; }
         public DateTime Deadline { get; set; }
-        public List<Task> Subtask { get; set; }
+        public List<int> Recur { get; set; } = new List<int>();
+        public List<Task> Subtask { get; set; } = new List<Task>();
         public bool IsInterruption { get; set; }
-        public bool IsCompleted { get; set; }
+
+        public bool IsCompleted
+        {
+            get => Subtask.Any() ? Subtask.All(_ => _.IsCompleted) : _isCompleted;
+            set => _isCompleted = value;
+        }
+
+        private bool _isCompleted = false;
+
+        public Task(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Must provide a valid name.");
+            }
+
+            Name = name;
+        }
     }
 }
