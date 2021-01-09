48fca38fdfe9ee224b0e31728b6c5fe9e1ea1836
YI ZHANG <scaccterran@gmail.com>
2021-01-09 16:31:44 -0500

    POM-13 allow subtask

--- DivisionWatchServer/Core/Models/TaskItem.cs
+++ DivisionWatchServer/Core/Models/TaskItem.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Core.Models
 {
@@ -13,16 +12,10 @@ namespace Core.Models
         public string Deadline { get; set; }
         public List<bool> Recur { get; set; } = new List<bool>();
         public List<EstimationResult> EstimationResults { get; set; } = new List<EstimationResult>();
-        public List<TaskItem> Subtasks { get; set; } = new List<TaskItem>();
+        public List<string> Subtasks { get; set; } = new List<string>();
+        public string Parent { get; set; } = null;
         public bool IsInterruption { get; set; }
-
-        public bool IsCompleted
-        {
-            get => Subtasks.Any() ? Subtasks.All(_ => _.IsCompleted) : _isCompleted;
-            set => _isCompleted = value;
-        }
-
-        private bool _isCompleted = false;
+        public bool IsCompleted { get; set; }
 
         public TaskItem()
         {
