fffcab8b9731b5f47160ac2fa12091a00e5c76f9
YI ZHANG <scaccterran@gmail.com>
2021-02-14 18:28:05 -0500

    POM-85 return task items by category

--- DivisionWatchServer/Service/Repositories/TaskItemRepository.cs
+++ DivisionWatchServer/Service/Repositories/TaskItemRepository.cs
@@ -18,6 +18,13 @@ namespace Service.Repositories
             return await Collection.Find(filter).Limit(limit).ToListAsync().ConfigureAwait(false);
         }
 
+        public async Task<IEnumerable<TaskItem>> GetTaskItemsByCategory(string categoryId)
+        {
+            var filter = Builders<TaskItem>.Filter.Eq(_ => _.CategoryId, categoryId);
+
+            return await Collection.Find(filter).ToListAsync().ConfigureAwait(false);
+        }
+
         public async Task<IEnumerable<TaskItem>> GetChildTaskItems(string parentId)
         {
             var filter = Builders<TaskItem>.Filter.Eq(_ => _.Parent, parentId);
