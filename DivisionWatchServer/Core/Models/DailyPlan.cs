e24909f63a857a4bcadb3a67fa17670aecb8ad9b
YI ZHANG <scaccterran@gmail.com>
2021-02-28 15:34:40 -0500

    POM-30 use better data structure for daily goal

--- DivisionWatchServer/Core/Models/DailyPlan.cs
+++ DivisionWatchServer/Core/Models/DailyPlan.cs
@@ -6,7 +6,7 @@ namespace Core.Models
     public class DailyPlan : DatabaseRecord
     {
         public DateTime Date { get; set; }
-        public int Goal { get; set; } = 1;
+        public Goal Goal { get; set; } = new Goal();
         public List<string> Planned { get; set; } = new List<string>();
         public List<string> Potential { get; set; } = new List<string>();
     }
